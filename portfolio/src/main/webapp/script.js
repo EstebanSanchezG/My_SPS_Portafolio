7ceee97fd1502f80eea8ca7b0614da42661dc0da
EstebanSanchezG <ssteve246@gmail.com>
2021-06-22 02:18:03 +0000

    Start of Week 3 activities with map. Add map, but it does not work

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,8 +1,17 @@
 
+
 async function showGreet() {
   const responseFromServer = await fetch('/hello');
   const textFromResponse = await responseFromServer.json();
 
   const greetContainer = document.getElementById('greeting');
   greetContainer.innerHTML = textFromResponse;
+}
+
+
+let map;
+
+function createMap() {
+  map = new google.maps.Map(document.getElementById('map'),
+      {center: {lat: 20.732606, lng: -103.455255}, zoom: 10});
 }
